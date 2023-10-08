0641b684ab6851a8d0e662429a611f5f8bd24c6f
IbrahimKHRMN <ibrahim.kahraman.4427@gmail.com>
2023-10-08 15:39:37 +0300

    Repositories dolduruldu.

---
+++ DataAccessLayer/Repositories/ProductDal.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstrack;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class ProductDal : IProductDal
+    {
+        Context c = new Context();
+        public void Delete(Product p)
+        {
+            c.Remove(p);
+            c.SaveChanges();
+        }
+
+        public List<Product> GetList()
+        {
+            return c.Products.ToList();
+        }
+
+        public void Insert(Product p)
+        {
+            c.Products.Add(p);
+            c.SaveChanges();
+        }
+
+        public void Update(Product p)
+        {
+            c.Products.Update(p);
+            c.SaveChanges();
+        }
+    }
+}
