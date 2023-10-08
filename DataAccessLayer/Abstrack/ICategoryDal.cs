0f8508cfbf761f7c4e3f698b71fd86e6cd2bfc75
IbrahimKHRMN <ibrahim.kahraman.4427@gmail.com>
2023-10-08 15:29:21 +0300

    Abstrack ve Interface ler.

---
+++ DataAccessLayer/Abstrack/ICategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstrack
+{
+    public interface ICategoryDal
+    {
+        void Insert(Category p);
+        void Update(Category p);
+        void Delete(Category p);
+        List<Category> GetList();
+    }
+}
---
+++ DataAccessLayer/Abstrack/ICustomerDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstrack
+{
+    public interface ICustomerDal
+    {
+        void Insert(Customer p);
+        void Update(Customer p);
+        void Delete(Customer p);
+        List<Customer> GetList();
+    }
+}
